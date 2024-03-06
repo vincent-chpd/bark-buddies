0c15a2e0f1d58ab8efcf9a6787ef86bd698cebe8
Vincent Chaussepied <vincent.chaussepied@gmail.com>
2024-03-06 07:50:29 +0000

    added faker and cloudinary gem and added data in seeds for user and dog

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,38 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+require "open-uri"
+
+User.destroy_all
+Dog.destroy_all
+
+puts "Creating users..."
+
+10.times do
+  file = URI.open('https://thispersondoesnotexist.com')
+  user = User.new(
+    name: Faker::Name.first_name,
+    email: Faker::Internet.email,
+    password: '123456',
+    location: Faker::Address.city,
+    age: rand(25..30),
+    bio: Faker::Lorem.paragraph
+  )
+  user.photo.attach(io: file, filename: 'dogowner.jpg', content_type: 'image/jpg')
+  user.save!
+  file.close
+end
+
+
+puts "Users created!"
+puts "Creating dogs..."
+
+10.times do Dog.create!(
+  name: Faker::Creature::Dog.name,
+  age: rand(1..10),
+  breed: Faker::Creature::Dog.breed,
+  gender: Faker::Gender.binary_type,
+  bio: Faker::Creature::Dog.meme_phrase,
+  user_id: User.all.sample.id
+)
+end
+
+puts "Dogs created!"
