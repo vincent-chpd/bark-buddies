c1e1be6035d8ca0546ed5c3344cea77d06b55eae
Vincent Chaussepied <vincent.chaussepied@gmail.com>
2024-03-11 14:07:16 +0000

    improve styling of profile and dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :show ]
 
   def home
     @communities = Community.all
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 end
