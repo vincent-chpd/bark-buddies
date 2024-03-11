c9bee8be0da32eb91f527cf24f319f25325cbfaa
Luca Gattamelata <luca.gattamelata.dev@gmail.com>
2024-03-11 19:27:30 +0000

    adding the defalut value of false to read attribute in the messages table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_09_130835) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_11_192051) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -94,7 +94,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_09_130835) do
     t.text "content"
     t.bigint "conversation_id", null: false
     t.bigint "user_id", null: false
-    t.boolean "read"
+    t.boolean "read", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["conversation_id"], name: "index_messages_on_conversation_id"
