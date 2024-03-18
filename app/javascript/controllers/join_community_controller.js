294208d5ade86a44ce3fb2423c4a847d9cc460b8
Vincent Chaussepied <vincent.chaussepied@gmail.com>
2024-03-18 17:36:55 +0000

    added toggle fefature to join and chat in communities

--- app/javascript/controllers/join_community_controller.js
+++ app/javascript/controllers/join_community_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="join-community"
 export default class extends Controller {
-  static targets = ["button"];
+  static targets = ["button", "chat"];
 
   toggle() {
     if (this.buttonTarget.textContent === "Join community") {
@@ -16,11 +16,13 @@ export default class extends Controller {
     this.buttonTarget.textContent = "Leave community";
     this.buttonTarget.style.backgroundColor = "red";
     this.buttonTarget.style.color = "white";
+    this.chatTarget.classList.remove("d-none");
   }
 
   leave() {
     this.buttonTarget.textContent = "Join community";
     this.buttonTarget.style.backgroundColor = "#e27835";
     this.buttonTarget.style.color = "black";
+    this.chatTarget.classList.add("d-none");
   }
 }
