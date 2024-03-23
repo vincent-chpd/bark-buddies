2d455e6d8b5fce5206449e353c7df7712806bbdf
Luca Gattamelata <luca.gattamelata.dev@gmail.com>
2024-03-23 12:50:25 +0000

    fixing the notifications bug

--- app/javascript/controllers/notifications_controller.js
+++ app/javascript/controllers/notifications_controller.js
@@ -1,27 +1,48 @@
 import { Controller } from "@hotwired/stimulus"
 import { createConsumer } from "@rails/actioncable"
 
-// Connects to data-controller="notifications"
 export default class extends Controller {
   static targets = [ "notificationsDot", "chatNotifications" ]
   connect() {
     this.channel = createConsumer().subscriptions.create(
       { channel: "NotificationChannel" },
-      { received: data => this.renderRedDot(data) }
-    )
+      { received: data => console.log(data) }
+      )
+
+    // this.updateNotificationsUI()
   }
 
-  renderRedDot(data) {
-    const notificationDot = this.notificationsDotTarget
-    const chatNotifications = this.chatNotificationsTarget
-    let unread_messages_number = data.data.unread_messages_number
-    const unreadMessagesNumber = unread_messages_number > 99 ? "99+" : unread_messages_number
-    if (unreadMessagesNumber) {
-      if (unreadMessagesNumber > 0) {
-        notificationDot.innerHTML += `<div class="notification-dot">${unreadMessagesNumber}</div>`
-        chatNotifications.innerHTML += `<div class="messages-dot">${unreadMessagesNumber}</div>`
-      }
-    }
-  }
+  // handleNotification(data) {
+  //   this.storeNotificationData(data)
+  //   this.updateNotificationsUI()
+  // }
 
+  // storeNotificationData(data) {
+  //   localStorage.setItem('notifications', JSON.stringify(data))
+  // }
+
+  // updateNotificationsUI() {
+  //   let data = JSON.parse(localStorage.getItem('notifications'))
+  //   this.renderRedDot(data)
+  // }
+
+  // renderRedDot(data) {
+  //   const notificationDot = this.notificationsDotTarget
+  //   const chatNotifications = this.chatNotificationsTarget
+
+  //   console.log(data.data.unread_messages_number)
+
+  //   if (data && data.data.unread_messages_number) {
+  //     const unreadMessagesNumber = data.data.unread_messages_number
+  //     const unreadMessagesDisplay = unreadMessagesNumber > 99 ? "99+" : unreadMessagesNumber
+
+  //     if (unreadMessagesNumber > 0) {
+  //       notificationDot.innerHTML += `<div class="notification-dot">${unreadMessagesDisplay}</div>`
+  //       chatNotifications.innerHTML += `<div class="messages-dot">${unreadMessagesDisplay}</div>`
+  //     } else {
+  //       notificationDot.innerHTML = '<%= link_to conversations_path, class: "icon" do %><i class="fas fa-envelope"></i><% end %>'
+  //       chatNotifications.innerHTML = '<%= link_to "Messages", conversations_path, class: "" %>'
+  //     }
+  //   }
+  // }
 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   has_one_attached :photo
   has_many :dogs, dependent: :destroy
-  has_many :conversations, foreign_key: :sender_id, dependent: :destroy
+  has_many :conversations, foreign_key: :sender_id
+  has_many :received_conversations, class_name: "Conversation", foreign_key: :recipient_id
   has_many :community_messages, dependent: :destroy
   has_many :sent_friendships, class_name: "Friendship", foreign_key: :sender_id, dependent: :destroy
   has_many :received_friendships, class_name: "Friendship", foreign_key: :receiver_id, dependent: :destroy
