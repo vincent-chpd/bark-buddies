da94bc3cc256021f914048eae50b3828fc79d9d8
Luca Gattamelata <luca.gattamelata.dev@gmail.com>
2024-03-31 20:23:56 +0100

    little fixes

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,7 +3,6 @@ class FriendshipsController < ApplicationController
   def create
     Friendship.create(sender_id: current_user.id, receiver_id: params[:receiver_id])
     other_user = User.find(params[:receiver_id])
-    flash[:notice] = "Following #{other_user.name}"
 
     redirect_to user_path(other_user)
   end
@@ -12,7 +11,7 @@ class FriendshipsController < ApplicationController
     @friendship = Friendship.find(params[:id])
     other_user = User.find(params[:receiver_id])
     @friendship.destroy
-    redirect_to user_path(other_user), notice: "Unfollowed #{User.find(@friendship.receiver_id).name}."
+    redirect_to user_path(other_user)
   end
 
   def index
