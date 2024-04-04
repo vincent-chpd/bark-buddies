0ce86087fb800c2515b3be1caeb6ddfc1d0c9b31
Vincent Chaussepied <vincent.chaussepied@gmail.com>
2024-04-04 15:59:56 +0100

    fix bug in homepage controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,10 +3,15 @@ class PagesController < ApplicationController
 
   def home
     @communities = Community.includes(photo_attachment: :blob).limit(10)
-    @users = User.includes(:dogs, photo_attachment: :blob).where.not(id: current_user.id)
     @user = current_user
+    if @user
+      @users = User.includes(:dogs, photo_attachment: :blob).where.not(id: @user.id)
+    else
+      @users = User.includes(:dogs, photo_attachment: :blob)
+    end
     @events = Event.includes(photo_attachment: :blob).limit(10)
   end
+
 
   def show
     @user = User.select(:id, :name, :email).find(params[:id])
