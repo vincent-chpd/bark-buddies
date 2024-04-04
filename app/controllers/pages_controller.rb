5db9ed763d7959047e01037d3cbade6cc43d1acc
Vincent Chaussepied <vincent.chaussepied@gmail.com>
2024-04-04 16:19:35 +0100

    fix bug in homepage view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,24 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :show]
+  skip_before_action :authenticate_user!, only: [:home, :show, :landing]
+  before_action :load_data, only: [:home]
 
   def home
-    @communities = Community.includes(photo_attachment: :blob).limit(10)
-    @user = current_user
-    if @user
-      @users = User.includes(:dogs, photo_attachment: :blob).where.not(id: @user.id)
-    else
-      @users = User.includes(:dogs, photo_attachment: :blob)
-    end
-    @events = Event.includes(photo_attachment: :blob).limit(10)
+    # Use instance variables set by before_action
   end
-
 
   def show
     @user = User.select(:id, :name, :email).find(params[:id])
   end
 
+  def landing
+  end
+
+  private
+
+  def load_data
+    @communities = Community.includes(photo_attachment: :blob).limit(10) # Limit the number of communities fetched
+    @users = User.includes(:dogs, photo_attachment: :blob).limit(10) # Limit the number of users fetched
+    @user = current_user
+    @events = Event.includes(photo_attachment: :blob).limit(10) # Limit the number of events fetched
+  end
 end
