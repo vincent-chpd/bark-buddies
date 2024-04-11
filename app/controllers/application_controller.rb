ca3ae81e3e8f300552ee3de099c9451edd6831d9
Vincent Chaussepied <vincent.chaussepied@gmail.com>
2024-04-11 21:08:19 +0100

    changed x-frame

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def default_headers
+    headers['X-Frame-Options'] = 'https://vincent-dev.netlify.app/'
+  end
+
+  private
+
+  def set_default_headers
+    response.headers['X-Frame-Options'] = 'ALLOW-FROM https://vincent-dev.netlify.app/'
+  end
 end
