84e6603d5212cdeb55f782395c293def1c29aefa
Vincent Chaussepied <vincent.chaussepied@gmail.com>
2024-04-12 09:07:23 +0100

    changed x-frame

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :set_x_frame_options
+  before_action :set_csp_header
 
   private
 
-  def set_x_frame_options
-    response.headers['X-Frame-Options'] = 'ALLOW-FROM https://vincent-dev.netlify.app/'
+  def set_csp_header
+    response.headers['Content-Security-Policy'] = "frame-ancestors https://vincent-dev.netlify.app/"
   end
 end
